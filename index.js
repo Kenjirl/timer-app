14c00b178cd8bf84957452a930a950220faa777f
Benedict Kenjiro <kenjiro442@gmail.com>
2022-10-26 05:57:36 +0800

    afalah

---
+++ index.js
@@ -0,0 +1,93 @@
+let hour = 0;
+let minute = 0;
+let second = 0;
+
+function increaseHour() {
+  if (hour === 24) hour = 0; else hour++;
+  $(".hour").text(hour);
+}
+
+function decreaseHour() {
+  if (hour === 0) hour = 24; else hour--;
+  $(".hour").text(hour);
+}
+
+function increaseMinute() {
+  if (minute === 59) minute = 0; else minute++;
+  $(".minute").text(minute);
+}
+
+function decreaseMinute() {
+  if (minute === 0) minute = 59; else minute--;
+  $(".minute").text(minute);
+}
+
+function increaseSecond() {
+  if (second === 59) second = 0; else second++;
+  $(".second").text(second);
+}
+
+function decreaseSecond() {
+  if (second === 0) second = 59; else second--;
+  $(".second").text(second);
+}
+
+function startCountdown() {
+  $(".increase-btn").attr("disabled", "disabled");
+  $(".decrease-btn").attr("disabled", "disabled");
+  $("#start-btn").attr("disabled", "disabled");
+  $("#stop-btn").removeAttr("disabled");
+  
+  let countdown = setInterval(function() {
+    if (second === 0) {
+      if (minute === 0) {
+        if (hour === 0) {
+          clearInterval(countdown);
+          stopCountdown();
+          $("#beep")[0].play();
+          return;
+        } else {
+          hour--;
+        }
+        minute = 59;
+      } else {
+        minute--;
+      }
+      second = 59;
+    } else {
+      second--;
+    }
+
+    $(".second").text(second);
+    $(".minute").text(minute);
+    $(".hour").text(hour);
+    
+  }, 1000);
+}
+
+function stopCountdown() {
+  hour = 0;
+  minute = 0;
+  second = 0;
+
+  $(".second").text(second);
+  $(".minute").text(minute);
+  $(".hour").text(hour);
+
+  $(".increase-btn").removeAttr("disabled");
+  $(".decrease-btn").removeAttr("disabled");
+  $("#start-btn").removeAttr("disabled");
+  $("#stop-btn").attr("disabled", "disabled");
+}
+
+function changeTheme() {
+  let attr = $("body").attr("data-theme");
+
+  if(typeof attr !== 'undefined' && attr !== false) {
+    $("body").removeAttr("data-theme");
+    $(".theme-btn").html(`<i class="fa-regular fa-sun"></i>`);
+  } else {
+    $("body").attr("data-theme", "dark");
+    $(".theme-btn").html(`<i class="fa-regular fa-moon"></i>`);
+  }
+}
